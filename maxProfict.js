85af4057f257578204bece2ef33417fcfaff5296
Santhiya .K <103623501+Santhiyasanthiya@users.noreply.github.com>
2024-02-16 09:29:33 +0530

    logic

---
+++ maxProfict.js
@@ -0,0 +1,72 @@
+let unit = 7;
+let maxtotal = 0;
+let maxsol = [];
+let buildtime = [4,5,10];
+let eachProfit= [1000,1500,3000];
+let sol = {
+    "P":0,
+    "T":0,
+    "C":0
+}
+
+let maxProfit = (flag) => {
+    let unitcopy = unit;
+    let profit = 0;
+    while(unitcopy>buildtime[0]){
+        if(flag==3){
+            if(unitcopy>buildtime[2]){
+                unitcopy-=buildtime[2];
+                profit+=(eachProfit[2]*unitcopy);
+                sol.C+=1;
+            }else if(unitcopy>buildtime[1]){
+                unitcopy-=buildtime[1];
+                profit+=(eachProfit[1]*unitcopy);
+                sol.T+=1;
+                console.log(sol)
+            }else{
+                unitcopy-=buildtime[0];
+                profit+=(eachProfit[0]*unitcopy);
+                sol.P+=1;
+            }
+        }else if(flag==2){
+            if(unitcopy>buildtime[1]){
+                unitcopy-=buildtime[1];
+                profit+=(eachProfit[1]*unitcopy); 
+                sol.T+=1;
+            }else{
+                unitcopy-=buildtime[0];
+                profit+=(eachProfit[0]*unitcopy);
+                sol.P+=1;
+            }
+        }else{
+            unitcopy-=buildtime[0];
+            profit+=(eachProfit[0]*unitcopy);
+            sol.P+=1;
+        }
+    }
+    if(maxsol.includes(sol)){
+        return;
+    }
+    if(maxtotal<profit){
+        maxtotal=profit;
+        maxsol=[]
+        maxsol.push(sol);
+    }else if(maxtotal==profit){
+        maxsol.push(sol)
+    }
+    sol = {
+    "p":0,
+    "T":0,
+    "C":0
+}
+}
+
+maxProfit(3);
+maxProfit(2);
+maxProfit(1);
+
+console.log("Earnings: "+maxtotal);
+console.log("Solutions");
+for(let i=1;i<maxsol.length;i++){
+    console.log("P: "+maxsol[i].P+" T: "+maxsol[i].T+" C: "+maxsol[i].C);
+}
